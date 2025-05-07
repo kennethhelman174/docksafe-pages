e22b051ec2b5354eb3b1d49771e680a1b65e2f75
kennethhelman174 <88667676+kennethhelman174@users.noreply.github.com>
2025-05-07 10:26:24 -0500

    Delete docksafe-github-pages directory

--- docksafe-github-pages/app.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById('logForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const output = document.getElementById('logOutput');
-  output.textContent = 'New trailer log recorded!';
-});
